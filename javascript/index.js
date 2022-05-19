9357d74ef9ef1002acd965109a54f07290540ebc
johyunjoo99 <chj17200@naver.com>
2022-05-19 19:41:29 +0900

    KAWS_2021.05.19

--- javascript/index.js
+++ javascript/index.js
@@ -1,53 +1,54 @@
-// slider 이동
 $(function(){
-function prev(){
-    $(".slider_ul li:last").prependTo('.slider_ul');
 
-    $('.slider_ul').css('margin-left',-1903);
-    $('.slider_ul').stop().animate({marginLeft: 0},800);
-}
 
-function next(){
-    $('.slider_ul').stop().animate({marginLeft: -1903}, function(){
-
-        $('.slider_ul li:first').appendTo('.slider_ul');
-        $('.slider_ul').css({marginLeft: 0});
-    });
-}
+// 메인 슬라이더
+var i=0;
 
 function slide(){
-    $('.slider_ul').stop().animate({marginLeft: -1903},1000, function(){
-
-        $('.slider_ul li:first').appendTo('.slider_ul');
-        $('.slider_ul').css({marginLeft: 0});
-    });
+    i++;
+    if(i > $('.slider_slide li:last').index()){
+        i=0;
+    }
+    $('.slider_slide li').eq(i).stop().show();
+    $('.slider_slide li').eq(i-1).stop().hide();
 }
 
-setInterval(slide, 4000);
+setInterval(slide, 300);
 
-$('.fa-chevron-left').click(function(){
-    prev();
+
+// 팝업 비디오
+$('.fa-play').click(function(){
+    $('#myModal').fadeIn(250);
 });
 
-$('.fa-chevron-right').click(function(){
-    next();
+$('.fa-xmark').click(function(){
+    $('#myModal').fadeOut(250);
 });
 
 
-// 줄거리 보기
-$('.fa-angle-down').click(function(){
-    $('#story_box2').css({height: 580});
-    $('.fa-angle-down').css({opacity: 0})
-    $('.fa-angle-down').css('transition-delay', '0s')
-    $('#story_box').css({height: 680});
+// 작품 슬라이드
+$('.fa-angle-left').click(function(){
+    $('#work_img').animate({marginLeft: 0});
+    $('.first').css('background-color', '#23222a');
+    $('.second').css('background-color', '#cacaca');
 });
 
-$('.fa-angle-up').click(function(){
-    $('#story_box2').css({height: 270});
-    $('#story_box2').css('transition-duration','0.5s');
-    $('.fa-angle-down').css({opacity: 1});
-    $('.fa-angle-down').css('transition-delay', '0.47s');
-    $('#story_box').css({height: 370});           
+$('.fa-angle-right').click(function(){
+    $('#work_img').animate({marginLeft: -1058});
+    $('.first').css('background-color', '#cacaca');
+    $('.second').css('background-color', '#23222a');
+});
+
+$('.first').click(function(){
+    $('#work_img').animate({marginLeft: 0});
+    $('.first').css('background-color', '#23222a');
+    $('.second').css('background-color', '#cacaca'); 
+});
+
+$('.second').click(function(){
+    $('#work_img').animate({marginLeft: -1058});
+    $('.first').css('background-color', '#cacaca');
+    $('.second').css('background-color', '#23222a');
 });
 
 });
